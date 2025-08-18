1ea61cad6ec4590c524a7296a08e132f4a312255
Neuber Oliveira <neuber.oliveira05@gmail.com>
2025-08-17 21:26:19 -0300

    Refatora componente ContactPhoneWA para usar tag <a> ao invés de evento onClick para abrir link no WhatsApp

--- src/components/ContactPhoneWA.js
+++ src/components/ContactPhoneWA.js
@@ -1,21 +1,14 @@
-"use client"
-
 import Image from 'next/image'
 
 export default function ContactPhoneWA(props) {
   const {name, phone} = props.contact
 
-  const handleOpenWhatsapp = (phone)=> ()=>{
-    const url = `https://wa.me/55${phone.replace(/\D/g, '')}?text=Olá, gostaria de conhecer o GC`
-    window.open(url, 'nb-whatsapp')
-  }
-
   return (
-    <div className="flex flex-row items-center cursor-pointer" onClick={handleOpenWhatsapp(phone)}>
+    <a className="flex flex-row items-center cursor-pointer" href={`https://wa.me/55${phone.replace(/\D/g, '')}?text=${encodeURIComponent('Olá, gostaria de conhecer o GC')}`}>
       <span>
         <span className="font-semibold">{name}:</span> <span className="">{phone}</span>
       </span>
       <Image alt="" src="/icons/external-link.svg" className="ml-1" width={16} height={16} />
-    </div>
+    </a>
   )
 }
