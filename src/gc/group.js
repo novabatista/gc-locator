0bcc5c2116265b80e37a0e6fa154b6782c8c53f8
Neuber Oliveira <neuber.oliveira05@gmail.com>
2025-09-12 20:54:44 -0300

    Refatora lógica de agrupamento e ordenação de GCs, extraindo funções para arquivos reutilizáveis e otimizando organização por distância e setor

---
+++ src/gc/group.js
@@ -0,0 +1,28 @@
+
+/**
+ * Sort GCs by distance from provided coordinates
+ * @param {Object} gcsData - The GCs data object (from gcs.json)
+ * @returns {Array} Sorted array of GCs with distances
+ */
+export function groupGCBySector(gcsData){
+  return Object.values(gcsData).reduce((acc, gc) => {
+    const sectorId = gc.sector.id
+    if (!acc[sectorId]) {
+      acc[sectorId] = []
+    }
+    acc[sectorId].push(gc)
+    acc[sectorId].sort((a, b) => a.name.localeCompare(b.name))
+    return acc
+  }, {})
+}
+
+/**
+ * Sort GCs by distance from provided coordinates
+ * @param {Object} gcsData - The GCs data object (from gcs.json)
+ * @returns {Array} Sorted array of GCs with distances
+ */
+export function groupGCBySectorFlat(gcsData){
+  const groupedBySector = Object.values(groupGCBySector(gcsData))
+
+  return groupedBySector.reduce((acc, gcs) => acc.concat(...gcs), [])
+}
--- src/gc/sort-by-distance.js
+++ src/gc/sort-by-distance.js
@@ -1,18 +1,15 @@
+import calculateDistance from '@/map/map'
+
 /**
  * Sort GCs by distance from provided coordinates
  * @param {{lat:number, lng:number}} coord - Latitude & Longitude to sort from
  * @param {Object} gcsData - The GCs data object (from gcs.json)
  * @returns {Array} Sorted array of GCs with distances
  */
-export default function sortGcsByDistance(coord) {
+export function sortGcsByDistance(coord, gcsData) {
   const {lat, lng} = coord;
   const gcsArray = Object.values(gcsData).map(gc => {
-    const distance = calculateDistance(
-      lat,
-      lng,
-      gc.address.lat,
-      gc.address.lng
-    );
+    const distance = calculateDistance({lat, lng}, gc.address);
 
     return {
       ...gc,
@@ -23,4 +20,16 @@ export default function sortGcsByDistance(coord) {
   const sortedGcs = gcsArray.sort((a, b) => a.distance - b.distance);
 
   return sortedGcs;
+}
+
+/**
+ * Sort GCs by distance from provided coordinates
+ * @param {{lat:number, lng:number}} coord - Latitude & Longitude to sort from
+ * @param {number} maxRadiusKm
+ * @param {Object} gcsData - The GCs data object (from gcs.json)
+ * @returns {Array} Sorted array of GCs with distances
+ */
+export function sortGcsByDistanceWithRadius(coord, maxRadiusKm, gcsData) {
+  const sortedByDistance = sortGcsByDistance(coord, gcsData);
+  return sortedByDistance.filter(gc => gc.distance < maxRadiusKm)
 }
