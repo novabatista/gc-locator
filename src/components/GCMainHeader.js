8b379f7fb426ce871be5df4ebdc4c51c785dff02
Neuber Oliveira <neuber.oliveira05@gmail.com>
2026-02-26 18:56:21 -0300

    Ajusta estilos e substitui logo no componente `GCMainHeader`
    Altera importação do logo para usar `logo-8.svg` e ajusta estilos visuais, incluindo alinhamento dos itens e tamanho do texto do slogan. Essa mudança melhora a aparência do cabeçalho e sua consistência com o design atualizado.

--- src/components/GCMainHeader.js
+++ src/components/GCMainHeader.js
@@ -1,15 +1,15 @@
-import GCLogo from '@/components/GCLogo'
+import GCLogo from '@/assets/logo-8.svg'
 
 export default function GCMainHeader(props) {
   return (
     <header className="flex flex-col mb-8">
-      <div className="flex flex-row items-center gap-2">
+      <div className="flex flex-row items-end gap-2">
         <GCLogo stroke="currentColor" className="" width={64} height={64}/>
         <h1 className="text-5xl ">
           <span className="uniform-black">GC</span> <small className="uniform">Nova Batista</small>
         </h1>
       </div>
-      <h2 className="uppercase font-black text-4xl">A igreja no <span className="text-accent">LAR</span></h2>
+      <h2 className="uppercase font-black text-[2.8rem]">A igreja no <span className="text-accent">LAR</span></h2>
     </header>
   )
 }
