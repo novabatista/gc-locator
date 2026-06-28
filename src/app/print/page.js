84142c33c6880aea52ba494683f50b41640e952e
Neuber Oliveira <neuber.oliveira05@gmail.com>
2026-06-28 10:25:42 -0300

    Ajusta layout e lógica de exibição na página de impressão
    Adiciona lógica condicional baseada no parâmetro `mode` para ajustar o zoom da página e o tamanho das imagens exibidas. Modifica espaçamentos, estilos e exibição condicional de seções como "contatos" e "reuniões", garantindo uma apresentação otimizada para diferentes modos de visualização.

--- src/app/print/page.js
+++ src/app/print/page.js
@@ -8,49 +8,44 @@ import database from '@/service/database/gcs'
 const MAP_STATIC_CONFIG = getMapStaticConfig()
 
 export default async function PageGCPrint({params, searchParams}) {
-  const shuffle = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]]
-    }
-    return array
-  }
-
+  const { mode } = await searchParams
+  const isModeDefault = mode !== 'min'
+  const pageZoom = isModeDefault ? 0.40 : 0.70
+  const imageSize = isModeDefault ? 320 : 300
 
   return (
-    <div className="font-sans w-full flex flex-row flex-wrap" style={{pageBreakAfter: 'always'}}>
+    <div className="font-sans w-full flex flex-row flex-wrap" style={{pageBreakAfter: 'always', zoom: pageZoom}}>
       {database.all().map((gc) => {
         const {id, name, address, contacts, schedules, links, config} = gc
-        const {lat, lng} = address.fake ?? address
 
         const imageAlt = `Mapa GC ${name}`
         const qrcodeImageUrl = `/qr/${id}.png?v=${MAP_STATIC_CONFIG.version}`
         const imageConfig = {
           alt: imageAlt,
-          width: 320,
-          height: 320,
+          width: imageSize,
+          height: imageSize,
           className: 'rounded-lg',
           src: qrcodeImageUrl,
         }
 
         return (
-          <div key={id} className="font-sans w-1/2 p-6 mb-10">
+          <div key={id} className="font-sans w-1/2 p-6 mb-4">
             <div className="flex flex-row justify-start gap-2">
-              <div className="flex flex-col w-6/12 gap-4">
+              <div className="flex flex-col justify-center w-6/12 gap-4">
                 <div className="flex flex-row items-center" style={{color: config.color.primary}}>
                   <GCLogo config={config} name={name} location="single" textSize="4xl" className="mr-2" width={64} height={64} />
                 </div>
 
-                <section className="flex flex-col justify-between">
+                {isModeDefault && <section id="contacts" className="flex flex-col justify-between">
                   <div className="">
                     <span className="text-xl" style={{color: config.color.primary}}>Líderes</span>
                     <div className="flex flex-row gap-6">
                       {contacts.map((contact, contactIndex) => <ContactPhoneWA key={contactIndex} contact={contact} name={name} classColor="" iconVisible={false} />)}
                     </div>
                   </div>
-                </section>
+                </section>}
 
-                <section>
+                {isModeDefault && <section id="schedule">
                   <span className="text-xl mb-1" style={{color: config.color.primary}}>Reuniões</span>
                   <div className="">
                     {schedules.map((schedule, scheduleIndex) => (
@@ -59,7 +54,7 @@ export default async function PageGCPrint({params, searchParams}) {
                       </div>
                     ))}
                   </div>
-                </section>
+                </section>}
 
                 <section id="address" className="flex flex-col">
                   <span className="text-xl mb-1" style={{color: config.color.primary}}>Onde acontece</span>
